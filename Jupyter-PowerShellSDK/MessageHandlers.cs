15a26d5cab6d3e9e75641672b3a6d00bf0532a7f
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2023-10-17 20:08:02 +0900

    PowerShellSDK 7.3.8

--- Jupyter-PowerShellSDK/MessageHandlers.cs
+++ Jupyter-PowerShellSDK/MessageHandlers.cs
@@ -18,7 +18,7 @@ namespace Jupyter_PowerShellSDK
                     LanguageInfo = new LanguageInfo
                     {
                         Name = "powershell",
-                        Version = "7.3.7",
+                        Version = "7.3.8",
                         MimeType = "application/x-powershell",
                         FileExtension = "ps1",
                         PygmentsLexer = "powershell",
